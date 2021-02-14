9e14eb541ba3241b2bcd2330199834a6a753ea2a
Srijan Gupta <gupta.srijan94@gmail.com>
2021-02-14 13:54:22 +0530

    UPDATED main.js and index page

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,10 @@
 function myfunction() {
+    requestAnimationFrame(myfunction);
     var elem = document.getElementById('pop');
     var r;
-    elem.style.transition = "2s";
-    r = Math.floor(Math.random() * 16777215).toString(16);
+    elem.style.transitionDuration = "200ms";
+    r = (Math.floor(Math.random() * 16777215)+500).toString(16);
     elem.style.color = "#" + r;
 }
+
+myfunction();
