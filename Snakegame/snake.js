d29016d2c934d35a3d794bfec764047c7b751757
Subhradeep10 <subhradeepsamanta2002@gmail.com>
2021-07-09 23:05:11 +0530

    SnakeGame folder is added

--- Snakegame/snake.js
+++ Snakegame/snake.js
@@ -1,9 +1,9 @@
 // Game Constants & Variables
 let inputDir = {x: 0, y: 0}; 
-const foodSound = new Audio('music/food.mp3');
-const gameOverSound = new Audio('music/gameover.mp3');
-const moveSound = new Audio('music/move.mp3');
-const musicSound = new Audio('music/music.mp3');
+const foodSound = new Audio('/assets/food.mp3');
+const gameOverSound = new Audio('/assets/gameover.mp3');
+const moveSound = new Audio('/assets/move/mp3');
+const musicSound = new Audio('/assets/music.mp3');
 let speed = 19;
 let score = 0;
 let lastPaintTime = 0;
