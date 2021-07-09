b25754ba978195a3419230b46fcaff912ffcb8c1
Subhradeep10 <subhradeepsamanta2002@gmail.com>
2021-07-09 21:24:29 +0530

    SnakeGame folder is modified

--- Snakegame/snake.js
+++ Snakegame/snake.js
@@ -1,3 +1,5 @@
 let direction = {x : 0, y : 0};
 let foodSound = new Audio('/assets/food.mp3');
 let gameOverSound = new Audio('/assets/gameover.mp3');
+let moveSound = new Audio('assets/move.mp3');
+let music = new Audio('assets/music.mp3')
