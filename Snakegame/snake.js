919a5ef6110eba878bcf7e0156a82586dba3ccb6
Subhradeep10 <subhradeepsamanta2002@gmail.com>
2021-07-09 22:10:59 +0530

    SnakeGame folder is modified

--- Snakegame/snake.js
+++ Snakegame/snake.js
@@ -4,10 +4,13 @@ const foodSound = new Audio('/assets/food.mp3');
 const gameOverSound = new Audio('/assets/gameover.mp3');
 const moveSound = new Audio('assets/move.mp3');
 const music = new Audio('assets/music.mp3');
+let speed = 2;
+let lastPaintTime = 0;
 
 //Game Functions
 function main(curtime) {
     window.requestAnimationFrame(main);
+    if((curtime - lastPaintTime)/1000 < 1/speed);
     console.log(curtime);
 }
 
