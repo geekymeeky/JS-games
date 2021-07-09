e27024cafab9c11e60610802063c8ef5a3e773c8
Subhradeep10 <subhradeepsamanta2002@gmail.com>
2021-07-09 21:58:33 +0530

    SnakeGame folder is modified

--- Snakegame/snake.js
+++ Snakegame/snake.js
@@ -1,5 +1,21 @@
-let direction = {x : 0, y : 0};
-let foodSound = new Audio('/assets/food.mp3');
-let gameOverSound = new Audio('/assets/gameover.mp3');
-let moveSound = new Audio('assets/move.mp3');
-let music = new Audio('assets/music.mp3')
+//Game Const
+let direction = { x: 0, y: 0 };
+const foodSound = new Audio('/assets/food.mp3');
+const gameOverSound = new Audio('/assets/gameover.mp3');
+const moveSound = new Audio('assets/move.mp3');
+const music = new Audio('assets/music.mp3');
+
+//Game Functions
+function main(curtime) {
+    window.requestAnimationFrame(main);
+    console.log(curtime);
+}
+
+
+
+
+
+
+
+//Game Logic
+window.requestAnimationFrame(main);
