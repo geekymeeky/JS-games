42c4ea0b138c4854c4fe6550cf77b1e2281c1390
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2021-07-12 12:27:04 +0530

    Update game.js

--- static/js/game.js
+++ static/js/game.js
@@ -68,7 +68,7 @@ function animatePress(currentColor) {
 }
 
 function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
+  var audio = new Audio("./sounds/" + name + ".mp3");
   audio.play();
 }
 
