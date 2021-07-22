3da6601a19e98be0bbbdea982e118af599beaddd
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2021-07-22 23:28:22 +0530

    Update dice.js

--- static/js/dice.js
+++ static/js/dice.js
@@ -19,7 +19,7 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
