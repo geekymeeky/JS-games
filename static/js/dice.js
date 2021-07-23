6698d7c2c0b31af3a011f9e3301936c123be45ec
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2021-07-23 19:26:24 +0530

    Update dice.js

--- static/js/dice.js
+++ static/js/dice.js
@@ -3,7 +3,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "./static/images/Dice-Game/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var randomImageSource = "static/images/Dice-Game/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -12,7 +12,7 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "./static/images/Dice-Game/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "static/images/Dice-Game/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
