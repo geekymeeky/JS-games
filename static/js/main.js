2af3ae746a7b69dbaf04599512228f6294e743d5
Srijan Kumar Gupta <gupta.srijan94@gmail.com>
2022-06-05 17:39:19 +0000

    update index.html

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,36 @@
 function myfunction() {
-    requestAnimationFrame(myfunction);
-    var elem = document.getElementById('pop');
-    var r;
-    elem.style.transitionDuration = "200ms";
-    r = (Math.floor(Math.random() * 16777215)+500).toString(16);
-    elem.style.color = "#" + r;
+  requestAnimationFrame(myfunction)
+  var elem = document.getElementById('pop')
+  var r
+  elem.style.transitionDuration = '200ms'
+  r = (Math.floor(Math.random() * 16777215) + 500).toString(16)
+  elem.style.color = '#' + r
 }
 
-myfunction();
-
+myfunction()
 
 function toggle_light_mode() {
-    var app = document.getElementsByTagName("HTML")[0];
-    var nav = document.getElementById("dm");
-    if (localStorage.lightMode == "dark") {
-      localStorage.lightMode = "light";
-      app.setAttribute("light-mode", "light");
-      nav.classList.remove("dark-theme");
-    } else {
-      nav.classList.add("dark-theme");
-      localStorage.lightMode = "dark";
-      app.setAttribute("light-mode", "dark");
-    }
+  var app = document.getElementsByTagName('HTML')[0]
+  var nav = document.getElementById('dm')
+  if (localStorage.lightMode == 'dark') {
+    localStorage.lightMode = 'light'
+    app.setAttribute('light-mode', 'light')
+    nav.classList.remove('dark-theme')
+  } else {
+    nav.classList.add('dark-theme')
+    localStorage.lightMode = 'dark'
+    app.setAttribute('light-mode', 'dark')
   }
-  
-  
-  window.addEventListener("storage", function () {
-    if (localStorage.lightMode == "dark") {
-      app.setAttribute("light-mode", "dark");
+}
+
+window.addEventListener(
+  'storage',
+  function () {
+    if (localStorage.lightMode == 'dark') {
+      app.setAttribute('light-mode', 'dark')
     } else {
-      app.setAttribute("light-mode", "light");
+      app.setAttribute('light-mode', 'light')
     }
-  }, false);
+  },
+  false
+)
